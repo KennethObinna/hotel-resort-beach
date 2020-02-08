538a76590ac9cad7c16377b8cd3e64cf75d62d4d
Nkwoji Kenneth <37513482+KennethObinna@users.noreply.github.com>
2020-02-08 15:36:23 +0100

    The Final Display2

--- src/Contentfuls.js
+++ src/Contentfuls.js
@@ -1,11 +1,11 @@
 import {createClient} from 'contentful'
-import {space_ID,access_Token} from './ytco/myData'
+//import {space_ID,access_Token} from './ytco/myData'
  
  
 
  export default createClient({
-    space:process.env.REACT_APP_API_SPACE, //"0plibpvgcaz1",
-    accessToken:process.env.REACT_APP_ACCESS_TOKEN//"2g8xtSsumzmfhZuQ9nZPAayg4j_6PFnYJcWBhL-W24Y"
+    space:process.env.REACT_APP_API_SPACE,
+    accessToken:process.env.REACT_APP_ACCESS_TOKEN
 
 })
 
@@ -14,8 +14,8 @@ import {space_ID,access_Token} from './ytco/myData'
     return(
         {
             createClient({
-                space:space_ID, //"0plibpvgcaz1",
-    accessToken:access_Token //"2g8xtSsumzmfhZuQ9nZPAayg4j_6PFnYJcWBhL-W24Y" 
+                space:space_ID, //"0",
+    accessToken:access_Token //"4" 
             })
         }
     )
--- src/ytco/myData.js
+++
@@ -1,3 +0,0 @@
- export const space_ID="0plibpvgcaz1";
- export const access_Token="2g8xtSsumzmfhZuQ9nZPAayg4j_6PFnYJcWBhL-W24Y";
-
